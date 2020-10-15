d88f7eb559c2ea52870953048754ea1f6b225af6
Oleg Solomka <olsolomk@microsoft.com>
2020-10-15 06:40:58 +0000

    minor copy change

--- src/utils/parseFileUrl.ts
+++ src/utils/parseFileUrl.ts
@@ -5,12 +5,7 @@ export const parseFileUrl = (fileUrl: string): IParsedFileUrl => {
   const { pathname } = uri;
 
   const split = pathname.split('/');
-
-  console.log(split);
-
   const path = decodeURIComponent(split.slice(4).join('/'));
-
-  console.log(path);
 
   return {
     owner: split[1],
